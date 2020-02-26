b900030fac573296265cdad6b1290c07ea18daa9
Cryothermus <evangrvy@gmail.com>
2020-02-25 20:24:51 -0700

    Initial commit of Assignment 2.

---
+++ cse360assign2/src/cse360assign2/SimpleList.java
@@ -0,0 +1,83 @@
+package cse360assign2;
+
+/*Name: Evan Garvey
+ * Student ID: 18600511
+ * Class ID: 143 (?)
+ * Assignment #: 1
+ * 
+ * 
+ * */
+// This class represent a SimpleList of flexible size, with up to 10 integers contained within.
+// The list works like a stack= the most recent item added is in position zero, with items added later being placed further back.
+public class SimpleList {
+		private int[] list;
+	    private int count;
+
+	    public SimpleList() 
+	    { //contains an array of variable size, with up to 10 integers
+	    	list = new int[10];
+	    	count = 0; //items past this are effectively nonexistent- data is here, but it won't be used for anything
+	    }
+
+	    public void add(int item /*item being added*/) 
+	    { 
+	    	// increments count, pushes all items forward 1 position in the array, and sets 0 to argument. Last item is pushed off and removed.
+	    	for (int inc = list.length - 1; inc > 0; inc--) 
+	    	{
+	    		list[inc] = list[inc - 1];
+	    	}
+	    	
+	    	list[0] = item;
+	    	
+	    	if (count < 10) count++; //makes sure count is larger than the array size
+	    }
+	    public void remove(int item /*item being removed*/) 
+	    { 
+	    	//removes the matching item (argument) from the list. All items past it are pushed back to "fill", and count is lowered by 1.
+	    	
+	    	int index = search(item); //this method is defined further down- it's repeated code, so I might as well
+	    	
+	    	if (index != -1) 
+	    	{
+	    		for (int inc = index; inc < count - 1; inc++) 
+	    		{
+	    			list[inc] = list[inc + 1];
+	    		}
+	    		
+	    		count--; //this only ticks down if search finds something, so no need for a check
+	    	}
+	    	
+	    }
+	    public int count() 
+	    { //returns count (number of items in the list).
+	    	
+	    	return count;
+
+	    }
+	    public String toString() 
+	    { // prints a string of all items in the list. Items are given in-order, and seperated by spaces.
+	    	
+	    	String output = "";
+	    	
+	    	for (int inc = 0; inc < count; inc++) 
+	    	{
+	    		if (inc > 0) output += " "; // adds blank space to seperate numbers
+	    		output += list[inc]; // adds numbers
+	    	}
+	    	
+	    	return output; //printed string of all items
+	    }
+	    
+	    public int search(int item /*item being searched for*/) 
+	    { //Searches for an item in the list and returns its index value. Returns -1 if not present.
+	    	
+	    	for (int inc = 0; inc < count; inc++) 
+	    	{
+	    		if (list[inc] == item) return inc; // if a match is found, it return the "i" value that contains it
+	    	}
+	    	
+	    	return -1; // returned if nothing is found
+	        
+	    }
+
+}
